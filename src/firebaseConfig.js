0d74058f0ede98fc0e8768df0ae107689c3f3b0b
SECRET-CHAMBER-WID <goni4001@gmail.com>
2026-05-01 13:56:39 +0900

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,9 @@
-// Firebase Console에서 웹 앱을 만든 뒤 이 값을 실제 config로 교체하세요.
-// 여러 휴대폰이 같은 게임에 접속하려면 Realtime Database가 반드시 켜져 있어야 합니다.
 export const firebaseConfig = {
-  apiKey: "PASTE_FIREBASE_API_KEY",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PASTE_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.appspot.com",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-  appId: "PASTE_APP_ID",
+  apiKey: "AIzaSyA9S7ik_ZQgbYKPGyOin1FmXmEWx0dvbQA",
+  authDomain: "secret-chamber-58dc1.firebaseapp.com",
+  databaseURL: "https://secret-chamber-58dc1-default-rtdb.firebaseio.com",
+  projectId: "secret-chamber-58dc1",
+  storageBucket: "secret-chamber-58dc1.firebasestorage.app",
+  messagingSenderId: "106201727974",
+  appId: "1:106201727974:web:6ad8dfcc8a1b53d03a0c27",
 };
